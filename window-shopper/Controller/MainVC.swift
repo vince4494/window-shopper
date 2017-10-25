d5f217455a7ad59648c262051a3c30f401609dae
vince zipparro <vincezipparro@vinces-mbp.lan>
2017-10-24 20:49:06 -0500

    created calculate button so it appears on top of decimal keyboard when textfields are pressed.

--- window-shopper/Controller/MainVC.swift
+++ window-shopper/Controller/MainVC.swift
@@ -10,16 +10,30 @@ import UIKit
 
 class MainVC: UIViewController {
 
-    override func viewDidLoad() {
+    @IBOutlet weak var wageTxt: CurrencyTxtField!
+    @IBOutlet weak var priceTxt: CurrencyTxtField!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 1, green: 0.5763723254, blue: 0, alpha: 1)
+        
+        //when working with buttons always use .setTitle to set the text
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+    
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    //@objc is a function that is  at objective-c runtime - happens when working with selectors
+    @objc func calculate()
+    {
+        print("we got here")
     }
-
 
 }
 
--- window-shopper/Views/CurrencyTxtField.swift
+++ window-shopper/Views/CurrencyTxtField.swift
@@ -8,16 +8,34 @@
 
 import UIKit
 
+
+//IBDesignable is a class attribute that allows changes made in the code to show up in the interface builder
+@IBDesignable
 class CurrencyTxtField: UITextField
 {
+    //view logic needs to happen in prepare for interface builder then
+    override func prepareForInterfaceBuilder()
+    {
+        customizeView()
+    }
+    
     override func awakeFromNib()
+    {
+        super.awakeFromNib()
+        
+        //call custom func
+        customizeView()
+    
+    }
+    
+    func customizeView()
     {
         super.awakeFromNib()
         backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
         layer.cornerRadius = 5.0
         textAlignment = .center
         
-       
+        
         
         if let p = placeholder
         {
